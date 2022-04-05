5131eb075d278d85e4244f3cedb7f9cc5ee4be8c
hweelu <hewwlu@naver.com>
2022-04-05 14:18:42 +0900

    modified

--- HelloGit/src/kr/co/ezenac/git/HelloGit.java
+++ HelloGit/src/kr/co/ezenac/git/HelloGit.java
@@ -3,7 +3,8 @@ package kr.co.ezenac.git;
 public class HelloGit {
 	
 	public static void main(String[] args) {
-		System.out.println("That's DVCS!");
+		System.out.println("Hello Git?");
+		System.out.println("Hello Git!");
 	}
 
 }
